f4accbe3cba71134d2ae33fbb8f65bfdc3d647dd
711119104030 <92197718+Harritha-s@users.noreply.github.com>
2022-08-09 13:45:29 +0530

    Add files via upload
    Finding the random of 3 elements in a array, storing them in a separate array and sort them according to first array

---
+++ random().py
@@ -0,0 +1,23 @@
+import random
+
+result = []
+index = []
+random_array = []
+array = list(map(int,input().strip().split()))
+for i in range(3):
+    r_value = random.choice(array) #finding random elements in array
+    random_array.append(r_value)
+print(random_array)
+for j in range(len(array)):
+    for k in range(len(random_array)):
+        if(array[j] == random_array[k]):
+            index.append(j)
+         
+index.sort()
+#print(index)
+
+for l in range(len(index)):
+    a = index[l]
+    result.append(array[a])
+    
+print(result)
