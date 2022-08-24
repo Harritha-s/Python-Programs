eeb4226f0ce4dbeadb458025714ee67313b26ecf
711119104030 <92197718+Harritha-s@users.noreply.github.com>
2022-08-24 19:22:10 +0530

    Update sum_of_array.py

--- sum_of_array.py
+++ sum_of_array.py
@@ -7,16 +7,21 @@ for i in range(n):
     a = int(input())
     array.append(a)
 for i in range(len(array)-1):
-    for j in range(i+1,n-1):
+    for j in range(i+1,n):
         total = array[i] + array[j]
+        if(total > sum):
+            break
         if(total == sum):
             print(i,j)
+            
             break
         else:
             while(j+1 < n):
                 total += array[j+1]
+                
                 if(total == sum):
                     print(i,j+1)
+                    
                 j += 1
 
 """ Output:
