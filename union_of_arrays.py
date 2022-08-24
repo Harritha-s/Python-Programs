96038f5205f47b7d8931fdae6ab061cc8e8a03fd
711119104030 <92197718+Harritha-s@users.noreply.github.com>
2022-08-24 20:05:38 +0530

    Update union_of_arrays.py

--- union_of_arrays.py
+++ union_of_arrays.py
@@ -1,1 +1,22 @@
+array1 = []
+array2 = []
+union = []
+n1 = int(input())
+n2 = int(input())
 
+for i in range(n1):           #creating arrays
+    a = int(input())
+    array1.append(a)
+print(array1)
+for i in range(n2):
+    b = int(input())
+    array2.append(b)
+print(array2)
+for i in range(n1):           #logic
+    if(array1[i] in array2 or array1[i] not in array2):
+        union.append(array1[i])
+for j in range(n2):
+    if(array2[j] not in array1):
+        union.append(array2[j])
+print(union)
+print(len(union))
